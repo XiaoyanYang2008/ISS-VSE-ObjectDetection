242b03ebf3c35e76a2d47f5e577d38ec96366b1a
tealeeseng <tealeeseng@gmail.com>
2019-10-30 23:03:26 +0800

    GPU 1 for quick test. Video generation script in GPU 0 add rmconfig as new rmconfig can be random after new training

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ conda list
 TIMESTAMP=`date +%Y%m%d-%H%M%S`
 
 # python3 FasterRCNNModel.py -m test -p VOCdevkit/VOC2012/test --network resnet50 --config_filename rmconfig.pickle --input_weight_path rm-model.hdf5 --output_weight_path rm-model.hdf5 
-python3 FasterRCNNModel.py -m test -p data/images --network resnet50 --config_filename rmconfig.pickle --input_weight_path rm-model.hdf5 
+python3 FasterRCNNModel.py -m test -p data/images --network resnet50 --gpu 1 --config_filename rmconfig.pickle --input_weight_path rm-model.hdf5 
 
